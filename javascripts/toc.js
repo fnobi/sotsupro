6ac5c6357dae4fcb707422c6da3e7677ba1d9d38
fnobi <shin@fnobi.com>
2013-01-03 15:37:56 +0900

    無駄に汎用性の高い目次システムを作ってしまった。

--- javascripts/toc.js
+++ javascripts/toc.js
@@ -1,42 +1,96 @@
 $(function () {
 	var TOC = function (selector) {
-		this.headers = [];
-
-		this.$rootElement = $(selector);
+		this.sections = [];
 	};
 
 	TOC.prototype.render = function () {
-		var $rootElement = this.$rootElement;
-		var headers = this.headers;
-		$rootElement.empty();
+		var $ul = this.$ul || $('<ul />');
+		$ul.empty();
 
-		var $ul = $('<ul />');
-		$rootElement.append($ul);
-
-		headers.forEach(function ($header) {
-			$ul.append($('<li />').append(
-				$('<a />')
-					.html($header.html())
-					.attr('href', '#' + $header.attr('id'))
-			));
+		this.sections.forEach(function (section) {
+			$ul.append(section.renderItem());
 		});
 
-		return $rootElement;
+		this.$ul = $ul;
+		return $ul[0];
 	};
 
-	TOC.prototype.addHeader = function ($header) {
-		var index = this.headers.length + 1;
+	TOC.prototype.addSection = function ($section) {
+		this.sections.push(new TOCSection(
+			$section,
+			this.sections.length + 1
+		));
+	};
+
+	var TOCSection = function ($section, index) {
+		this.$section = $section;
+		this.index = index;
+		this.initHeader();
+		this.initSubSections();
+	};
+
+	TOCSection.prototype.initHeader = function () {
+		var $section = this.$section;
+		var $header = $('h1', $section).first();
+		var index = this.index;
 
 		$header.attr('id', 'section' + index);
 		$header.prepend(index + ' ');
-		this.headers.push($header);
+
+		this.$header = $header;
+		return $header[0];
 	};
 
-	var toc = new TOC('#toc');
+	TOCSection.prototype.initSubSections = function () {
+		var self = this;
+		var $section = this.$section;
+		var indexpre = self.index + '-';
+		var subSections = [];
 
-	$('article h1').each(function () {
-		toc.addHeader($(this));
+		if ($('section', $section)[0]) {
+			$('section', $section).each(function () {
+				subSections.push(new TOCSection(
+					$(this),
+					indexpre + (subSections.length + 1)
+				));
+			});
+		}
+
+		this.subSections = subSections;
+		return subSections;
+	};
+
+	TOCSection.prototype.renderItem = function () {
+		if (this.$item) {
+			return this.$item[0];
+		}
+
+		var $header = this.$header;
+		var $item = $('<li />').append(
+			$('<a />')
+				.html($header.html())
+				.attr('href', '#' + $header.attr('id'))
+		);
+
+		var subSections = this.subSections;
+
+		if (subSections.length) {
+			var $list = $('<ul />');
+			subSections.forEach(function (subSection) {
+				$list.append(subSection.renderItem());
+			});
+			$item.append($list);
+		}
+
+		this.$item = $item;
+		return $item[0];
+	};
+
+	var toc = new TOC();
+
+	$('article > section').each(function () {
+		toc.addSection($(this));
 	});
 
-	toc.render();
+	$('#toc').append(toc.render());
 });
